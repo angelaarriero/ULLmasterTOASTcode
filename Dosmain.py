e973f77be317b91ab34956989851840620c97464
angelaarriero <46733726+angelaarriero@users.noreply.github.com>
2023-05-11 14:16:50 +0100

    test

---
+++ Dosmain.py
@@ -0,0 +1,38 @@
+import numpy as np
+import healpy as hp
+from toast.tod import AnalyticNoise
+import os
+#####-------##########
+from TODangela import (TODGb)
+import classesDef as cd
+########----##############
+import pickle
+#CMB INPUT MAP
+MAPA_SIM= cd.cmbinput(cd.Dsimulation)
+#prueba
+#to change the polarization angles of the detectors
+cd.Ddetector.detquat['2']=[0.027150201580442457,0.0028586627064642166,0.38116388211451296,0.9241043174734472]
+cd.Ddetector.detquat['0']=[0.01545194635831245,-0.003324119502272795,0.923267816842732,0.3838316375257909]
+cd.Ddetector.detquat['7']=[0.01954527152183795,-0.030073028133292462,0.922745744117246,0.3837367418602782]
+cd.Ddetector.detquat['12']=[0.02913085238689356,-0.012026206551948448,0.3814051931803069,0.9238705766557758]
+cd.Ddetector.detquat['10']=[0.006333093068507152,-0.0152386367540685,0.9233023087595494,0.38371932779226653]
+cd.Ddetector.detquat['17']=[-0.004648629965548112,0.028130068744699158,-0.9231219361253306,-0.3834487977762909]
+cd.Ddetector.detquat['22']=[-0.031104438072555272,0.026928211119763415,-0.38174173204247064,-0.9233529311131766]
+cd.Ddetector.detquat['20']=[0.0027946836751364147,0.02715686177532145,-0.9232036569137106,-0.38334019155682547]
+
+#1/F AND WHITE NOISE FUNCTION---> KNEE FRECUENCY VALUE, NET value, fmin
+N2=cd.minoise(cd.Ddetector,250,1) #1/f 1Hz
+
+weth_fil=cd.Dschedule.weather_atacama
+outdir2="/scratch/aarriero/main_docs/resultados/map_maker_test1"
+outprefix2="toast_test_"
+
+Noise1,data1=cd.mifuncion(cd.Ddetector,cd.Dschedule,N2,weth_fil,cd.focalplane,
+                            MAPA_SIM,cd.Dsimulation,outdir2,outprefix2)
+
+
+with open('noise1.pickle', 'wb') as handle:
+    pickle.dump(Noise1, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+with open('noise1.pickle', 'rb') as handle:
+    Noise1_pkl = pickle.load(handle)
--- graficas.py
+++ graficas.py
@@ -4,14 +4,14 @@ from astropy.io import fits
 import numpy as np
 import matplotlib.pyplot as plt
 import healpy as hp
-from tqdm import tqdm
+#from tqdm import tqdm
 
 
-hits_map = hp.read_map("/scratch/aarriero/main_docs/resultados/map_maker_test1/toast_test_hits.fits",field=None)
+hits_map = hp.read_map("/scratch/aarriero/main_docs/resultados/map_maker_test1/toast_test_hits.fits", field=None)
 hits_map[hits_map == 0] = hp.UNSEEN
-destriped = hp.read_map("/scratch/aarriero/main_docs/resultados/map_maker_test1/toast_test_destriped.fits",field=None)
+destriped = hp.read_map("/scratch/aarriero/main_docs/resultados/map_maker_test1/toast_test_destriped.fits", field=None)
 destriped[destriped == 0] = hp.UNSEEN
-binned = hp.read_map("/scratch/aarriero/main_docs/resultados/map_maker_test1/toast_test_binned.fits",field=None)
+binned = hp.read_map("/scratch/aarriero/main_docs/resultados/map_maker_test1/toast_test_binned.fits", field=None)
 binned[binned == 0] = hp.UNSEEN
 plt.figure(figsize=[12, 12])
 hp.mollview(hits_map[0], sub=[1, 3, 1], title="24h Hits Map: 1/f Noise",unit='uK')
